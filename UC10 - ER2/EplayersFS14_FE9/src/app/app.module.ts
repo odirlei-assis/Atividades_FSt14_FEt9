b359f47beb10078ac1aebcfe413b3e9a2fd9ced7
Odirlei <odirlei_sabella@hotmail.com>
2022-10-26 15:51:03 -0300

    adicionado ER2 UC10

---
+++ UC10 - ER2/EplayersFS14_FE9/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { LoginComponent } from './views/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ UC10 - ER2/EplayersFS14_FE9/src/app/views/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
+import { HomeComponent } from '../home/home.component';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private loginService: LoginService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  userModel = new User();
+
+  mensagem = "";
+
+  receberDados(){
+    this.loginService.login(this.userModel).subscribe({
+      next: (response) => { 
+        console.log(response.body.user.nome)
+        localStorage.setItem("nomeUsuario", response.body.user.nome)
+        this.router.navigateByUrl("/")
+      }, error: (msgErro) => {
+        console.log(msgErro.error);
+        this.mensagem = msgErro.error
+        // alert(msgErro.error)
+
+        if (msgErro.error == "Password is too short") {
+          this.mensagem = "Senha muito curta";
+        }else{
+          this.mensagem = msgErro.error
+        }
+
+
+      }
+    })
+  }
+
+}
